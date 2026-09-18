e7c454086211033e06e2d2f6605efd7d2f2407a8
Kasundi Vethmini <kasundivethmini@gmail.com>
2026-09-18 13:15:29 +0530

    Add ItemType enum for resource categorization

---
+++ backend/Aegis.Resource/Entities/ItemType.cs
@@ -0,0 +1,12 @@
+namespace Aegis.Resource.Entities
+{
+    public enum ItemType
+    {
+        Food,
+        Medical,
+        Shelter,
+        RescueEquipment,
+        Fuel,
+        Other
+    }
+}
